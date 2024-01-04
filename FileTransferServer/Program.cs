447ebb1dc51cee9da21a15180f78bcd2e05d785c
BingFengHung <joe94008@gmail.com>
2024-01-04 09:36:49 +0800

    <feat>: 可接受多檔案傳輸

--- FileTransferServer/Program.cs
+++ FileTransferServer/Program.cs
@@ -66,52 +66,57 @@ namespace FileTransferServer
 
             try
             {
-                // 接收檔案名稱長度
-                bytesRead = ReadFull(stream, buffer, 0, 4);
-
-                if (bytesRead != 4)
-                    throw new InvalidOperationException("無法從網路流中讀取完整的檔案名稱長度資訊");
-
-                int fileNameLen = BitConverter.ToInt32(buffer, 0);
-
-                // 接收檔案名稱
-                bytesRead = ReadFull(stream, buffer, 0, fileNameLen);
-                if (bytesRead != fileNameLen)
-                    throw new InvalidOperationException("無法從網路流中讀取完整的檔案名稱");
-                string fileName = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesRead);
-
-                // 接收檔案大小
-                bytesRead = ReadFull(stream, buffer, 0, 4);
-
-                if (bytesRead != 4)
-                    throw new InvalidOperationException("無法從網路流中讀取完整的檔案大小");
-                int fileDataLen = BitConverter.ToInt32(buffer, 0);
-                Console.WriteLine(fileDataLen);
-
-                if (fileDataLen < 0)
+                while (true)
                 {
-                    throw new InvalidOperationException("檔案大小不能為負數。");
-                }
 
-                // 創建檔案
-                string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
-                using (var fileStream = new FileStream(filePath, FileMode.Create))
-                {
-                    int totalBytesRead = 0;
-                    while (totalBytesRead < fileDataLen)
+                    // 接收檔案名稱長度
+                    bytesRead = ReadFull(stream, buffer, 0, 4);
+
+                    if (bytesRead != 4)
+                        throw new InvalidOperationException("無法從網路流中讀取完整的檔案名稱長度資訊");
+
+                    int fileNameLen = BitConverter.ToInt32(buffer, 0);
+
+                    if (fileNameLen == -1) // 結束了
+                        break;
+
+                    // 接收檔案名稱
+                    bytesRead = ReadFull(stream, buffer, 0, fileNameLen);
+                    if (bytesRead != fileNameLen)
+                        throw new InvalidOperationException("無法從網路流中讀取完整的檔案名稱");
+                    string fileName = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                    // 接收檔案大小
+                    bytesRead = ReadFull(stream, buffer, 0, 4);
+
+                    if (bytesRead != 4)
+                        throw new InvalidOperationException("無法從網路流中讀取完整的檔案大小");
+                    int fileDataLen = BitConverter.ToInt32(buffer, 0);
+                    Console.WriteLine(fileDataLen);
+
+                    if (fileDataLen < 0)
                     {
-                        int toRead = Math.Min(buffer.Length, fileDataLen - totalBytesRead);
-                        bytesRead = ReadFull(stream, buffer, 0, toRead);
+                        throw new InvalidOperationException("檔案大小不能為負數。");
+                    }
 
-                        if (bytesRead == 0)
+                    // 創建檔案
+                    string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        int totalBytesRead = 0;
+                        while (totalBytesRead < fileDataLen)
                         {
-                            throw new InvalidOperationException("網絡流提前結束，無法讀取完整的檔案數據。");
+                            int toRead = Math.Min(buffer.Length, fileDataLen - totalBytesRead);
+                            bytesRead = ReadFull(stream, buffer, 0, toRead);
+
+                            if (bytesRead == 0)
+                            {
+                                throw new InvalidOperationException("網絡流提前結束，無法讀取完整的檔案數據。");
+                            }
+
+                            fileStream.Write(buffer, 0, bytesRead);
+                            totalBytesRead += bytesRead;
                         }
-
-                        fileStream.Write(buffer, 0, bytesRead);
-                        totalBytesRead += bytesRead;
-
-                        //totalBytesRead += bytesRead;
                     }
                 }
             }
